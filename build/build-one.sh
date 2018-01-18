f38154bf5d14faad5dea10783a855ee496992275
Daniel Castañeda <daniel@kwantec.com>
2018-01-18 13:11:24 -0600

    Modified docker images to use non root user

--- build/build-one.sh
+++ build/build-one.sh
@@ -4,9 +4,39 @@ export TAG=2.0.171212
 export UI_TAG=2.0.180108
 export MEMORY=8g
 export CPUS=4
+export MYUID=1001
+
+print_usage(){
+  echo "Missing required parameter, Usage:"
+  echo "    ./build-one all <bitshares user PWD>     Builds Docker images for ALL Bitshares Projects"
+  echo "                                                 'bitshares user PWD' will be the unencrypted password"
+  echo "                                                 for the linux user 'bitshares' that will be created inside"
+  echo "                                                 the Ubuntu in the images to run the processes"
+  echo "                                                 (in order to not run as root for security)"
+  echo "    ./build-one core <bitshares user PWD>    Builds the base Node docker image. "
+  echo "                                                This be done before the others as it is used as"
+  echo "                                                base Docker image for the rest of the images."
+  echo "    ./build-one cli           Builds the Docker image for the Bitshares cli-wallet"
+  echo "    ./build-one delayed       Builds the Docker image for the Bitshares delayed-node"
+  echo "    ./build-one full          Builds the Docker image for the Bitshares full-node"
+  echo "    ./build-one ngnix         Builds the Docker image for a nginx Web Server"
+  echo "                                    This is not really required as it is also done in the web-full image"
+  echo "                                    We recommend to use the web-full option instead"
+  echo "    ./build-one web           Builds the Docker image for the Bitshares-ui Web Project, "
+  echo "                                    this includes the nginx web server in the image as well "
+  echo "                                    as the web-ui nodejs project build."
+  echo "     Script by Daniel Castaneda <daniel@kwantec.com>"
+}
 
 case $1 in
   core)
+      #if [ -z "$2" ]
+      #  then
+      #    echo "Missing <bitshares user PWD> parameter "
+      #    print_usage
+      #    exit 1
+      #fi
+
       echo "Building dock the Base Node Docker Image for BitShares"
       echo "     This is the Base Image for most of the rest of the"
       echo "     Docker images produced by this script,"
@@ -18,7 +48,8 @@ case $1 in
       echo "     Docker container CPUs: ${CPUS}"
       echo "This will take a while, please be patient"
       echo "  script by Daniel Castaneda <daniel@kwantec.com>"
-      docker build -m ${MEMORY} -c ${CPUS} -t bitshares/bitshares-2 --build-arg tag=${TAG} bitshares-2
+      #docker build -m ${MEMORY} -c ${CPUS} -t bitshares/bitshares-2 --build-arg tag=${TAG} --build-arg user_pwd=$2 bitshares-2
+      docker build -m ${MEMORY} -c ${CPUS} -t bitshares/bitshares-2:${TAG} --build-arg tag=${TAG} --build-arg myuid=${MYUID} bitshares-2
     ;;
   cli)
       echo "Building the cli-walllet Docker image for BitShares"
@@ -80,6 +111,12 @@ case $1 in
       docker build -m ${MEMORY} -c ${CPUS} -t bitshares/bitshares-2-webwallet-full --build-arg ui_tag=${UI_TAG} bitshares-2-webwallet-full
     ;;
   all)
+      # if [ -z "$2" ]
+      #   then
+      #    echo "Missing <bitshares user PWD> parameter "
+      #     print_us  e
+      #    exit 1
+      #fi
       echo "Building docker images for ALL BitShares projects"
       echo "Parameters:"
       echo "     Version/tag for bitshares-2: ${TAG}"
@@ -89,7 +126,8 @@ case $1 in
       echo "This will take a while, please be patient"
       echo "  script by Daniel Castaneda <daniel@kwantec.com>"
       echo "============STARTING CORE IMAGE====================================================================="
-      docker build -m ${MEMORY} -c ${CPUS} -t bitshares/bitshares-2 --build-arg tag=${TAG} bitshares-2
+      #docker build -m ${MEMORY} -c ${CPUS} -t bitshares/bitshares-2 --build-arg tag=${TAG} --build-arg user_pwd=$2 bitshares-2
+      docker build -m ${MEMORY} -c ${CPUS} -t bitshares/bitshares-2:${TAG} --build-arg tag=${TAG} --build-arg myuid=${MYUID} bitshares-2
       echo "============STARTING CLI WALLET IMAGE==============================================================="
       docker build -m ${MEMORY} -c ${CPUS} -t bitshares/bitshares-2-cli-wallet --build-arg tag=${TAG} bitshares-2-cli-wallet
       echo "============STARTING DELAYED NODE IMAGE============================================================="
@@ -103,21 +141,7 @@ case $1 in
       echo "============DONE BUILDING IMAGES (pl. verify if all went well)======================================"
     ;;
   *)
-      echo "Missing required parameter, Usage:"
-      echo "    ./build-one all           Builds Docker images for ALL Bitshares Projects"
-      echo "    ./build-one core          Builds the base Node docker image. "
-      echo "                                    This be done before the others as it is used as"
-      echo "                                    base Docker image for the rest of the images."
-      echo "    ./build-one cli           Builds the Docker image for the Bitshares cli-wallet"
-      echo "    ./build-one delayed       Builds the Docker image for the Bitshares delayed-node"
-      echo "    ./build-one full          Builds the Docker image for the Bitshares full-node"
-      echo "    ./build-one ngnix         Builds the Docker image for a nginx Web Server"
-      echo "                                    This is not really required as it is also done in the web-full image"
-      echo "                                    We recommend to use the web-full option instead"
-      echo "    ./build-one web           Builds the Docker image for the Bitshares-ui Web Project, "
-      echo "                                    this includes the nginx web server in the image as well "
-      echo "                                    as the web-ui nodejs project build."
-      echo "     Script by Daniel Castaneda <daniel@kwantec.com>"
+      print_usage
 
     ;;
 
