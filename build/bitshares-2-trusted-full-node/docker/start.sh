380cac0cde6e9cd434469691994c661fb075e190
Daniel Castañeda <daniel@kwantec.com>
2018-01-22 20:23:12 -0600

    Added optional parameters for witness id and keys

--- build/bitshares-2-trusted-full-node/docker/start.sh
+++ build/bitshares-2-trusted-full-node/docker/start.sh
@@ -2,8 +2,22 @@
 
 #RUNLOG="/data/run-logs/runlog_$(date '+%Y-%m-%d_%H.%M.%S')"
 
-/bitshares-2/programs/witness_node/witness_node \
-                       --p2p-endpoint="0.0.0.0:9090" \
-                       --data-dir=/data/ \
-                       --rpc-endpoint="0.0.0.0:8090"
-                       #> ${RUNLOG} &
+if [[ -n $1 ] && [ -n $2 ] && [ -n $3 ]];
+  then
+    # All three parameters were supplied. run in witness mode
+    echo "All 3 parameters were found so RUNNING IN WITNESS MODE with witness-id: $1"
+    /bitshares-2/programs/witness_node/witness_node \
+                           --p2p-endpoint="0.0.0.0:9090" \
+                           --data-dir=/data/ \
+                           --rpc-endpoint="0.0.0.0:8090" \
+                           --witness-id '"$1"' \
+                           --private-key '["$2","$3"]'
+
+  else
+    # Some parameter is not there, so run without witness mode
+    echo "Not all 3 parameters were found so running WITHOUT witness ID and Keys"
+    /bitshares-2/programs/witness_node/witness_node \
+                           --p2p-endpoint="0.0.0.0:9090" \
+                           --data-dir=/data/ \
+                           --rpc-endpoint="0.0.0.0:8090"
+fi
